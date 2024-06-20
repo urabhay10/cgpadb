c7bdc79a181b4c1ff1513ffd6cc247fc0c6c683a
Abhay Upadhyay <urabhay10@gmail.com>
2024-06-20 20:09:42 +0530

    sem 2 updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Mainpage from './Components/Mainpage';
 import Results from './Components/Results';
 import './App.css';
